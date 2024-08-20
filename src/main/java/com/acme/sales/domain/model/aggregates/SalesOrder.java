d0f27a67f006d0cd9a39fe8d10beb36c9731d86b
Anatoly69420 <u202211813@upc.edu.pe>
2024-08-19 22:38:46 -0500

    feat:"mandando un commit telepatico"

--- src/main/java/com/acme/sales/domain/model/aggregates/SalesOrder.java
+++ src/main/java/com/acme/sales/domain/model/aggregates/SalesOrder.java
@@ -35,7 +35,7 @@ public class SalesOrder {
         this.paymentAmount+=amount;
         verifyIfReadyForDispatch();
     }
-    //ayuda2 mejorado
+    //ayuda2 mejorado puto el que lo lea
     public void dispatch(String street, String number, String city, String state,String zipCode, String country){
         verifyIfReadyForDispatch();
         this.shippingAddress=new Address(street,number,city,state,zipCode,country);
